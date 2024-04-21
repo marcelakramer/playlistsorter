4a43ccdd19bf98d770fced985cbd0471a78dbefd
marcelakramer <101991467+marcelakramer@users.noreply.github.com>
2024-04-21 19:36:18 -0300

    feat: create callback page

---
+++ pages/callback/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="h-screen bg-primary d-flex flex-column align-center pt-16">
+        <h1 class="mt-16 font-title text-center">
+            Redirecting...
+        </h1>
+        <v-progress-circular class="mt-12" indeterminate :size="70"></v-progress-circular>
+    </div>
+</template>
