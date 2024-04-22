07e6e419cf92157dc6617e7dc690cfa059ff16b4
Marcela Kramer <marcelabarretokramer10@gmail.com>
2024-04-22 10:50:49 -0300

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="d-flex flex-column">
+        <div class="bg-primary d-flex justify-end pt-6 pr-16">
+            <v-btn class="bg-primary" flat icon="mdi-palette" @click="toggleTheme"></v-btn>
+        </div>
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+function toggleTheme () {
+    theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
